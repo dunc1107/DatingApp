4a0a3c41294c1f86c275316f08968f0c7165d7a1
dunc1107 <dunc1107@hotmail.com>
2020-01-19 15:48:02 +0000

    End of Section 5 - correction to error handling

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -1,36 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import {HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req).pipe(
-            catchError(error => {
-                if (error instanceof HttpErrorResponse) {
-                    const applicationError = error.headers.get('Application-Error');
-                    if (applicationError) {
-                        console.error(applicationError);
-                        return throwError(applicationError);
-                    }
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error.status === 401) {
+          return throwError(error.statusText);
+        }
 
-                    /// If not modal state Error (error from Model classes in API) then show serverError
-                    const serverError = error.error;
-                    let modalStateErrors = '';
-                    if (serverError && typeof serverError === 'object') {
-                        for (const key in serverError) {
-                            if (serverError[key]) {
-                                modalStateErrors += serverError[key] + '\n';
-                            }
-                        }
-                    }
-                    return throwError(modalStateErrors || serverError || 'Server Error');
+        if (error instanceof HttpErrorResponse) {
+          const applicationError = error.headers.get('Application-Error');
+          if (applicationError) {
+            return throwError(applicationError);
+          }
 
-                }
-            })
-        );
-    }
+          /// If not model state Error (error from Model classes in API) then show serverError
+          const serverError = error.error;
+          let modelStateErrors = '';
+          if (serverError.errors && typeof serverError.errors === 'object') {
+            for (const key in serverError.errors) {
+              if (serverError.errors[key]) {
+                modelStateErrors += serverError.errors[key] + '\n';
+              }
+            }
+          }
+
+          return throwError(modelStateErrors || serverError || 'Server Error');
+        }
+      })
+    );
+  }
 }
 
 export const ErrorInterceptorProvider = {
